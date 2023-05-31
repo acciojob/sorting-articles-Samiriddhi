cd5979c0f5f8f1f5486eb21be6a2383d23be691c
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-05-31 20:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required.
-    const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+  
+
+/*const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
      
     console.log("script")
      
@@ -18,4 +20,4 @@
     document.querySelector("#bands").innerHTML = 
         sortedBands.map(band=> `<li>${band}</li>`).join("")
      
-    console.log(sortedBands);
+    console.log(sortedBands);*/
